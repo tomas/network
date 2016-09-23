4d7bb26d703b7c4b7e697c3b1ba4f1d339274d7f
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2016-09-23 14:33:29 -0400

    Update needle dependency and update test file to call valid functions

--- test.js
+++ test.js
@@ -1,26 +1,26 @@
-var network = require('./');
+var network = require('./lib');
 
 network.get_public_ip(function(err, res) {
   console.log('Public IP --- ');
   console.log(err || res);
-})
+});
 
 network.get_private_ip(function(err, res) {
   console.log('Private IP --- ');
   console.log(err || res);
-})
+});
 
 network.get_gateway_ip(function(err, res) {
   console.log('Gateway IP --- ');
   console.log(err || res);
-})
+});
 
-network.get_active_network_interface(function(err, res) {
+network.get_active_interface(function(err, res) {
   console.log('Active Network Interface --- ');
   console.log(err || res);
-})
+});
 
-network.get_network_interfaces_list(function(err, res) {
+network.get_interfaces_list(function(err, res) {
   console.log('Network Interfaces List --- ');
   console.log(err || res);
-})
+});
